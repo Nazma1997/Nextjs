d4587a01cce4302238053907fc4aa207fcf567a4
Nazma1997 <nazma.ist.707@gmail.com>
2022-10-08 23:20:32 +0600

    added

---
+++ data-fetching/pages/post/index.js
@@ -0,0 +1,31 @@
+import Link from "next/link"
+
+
+export default function PostList({posts}){
+  return(
+    <div>
+      <h1>All The Post</h1>
+      {posts.map(post => (
+        <div key={post.id}>
+          <Link href={`/posts/${post.id}`} passHref>
+            <h1>{post.id} . {post.title}</h1>
+          </Link>
+
+        </div>
+      ))}
+    </div>
+  )
+};
+
+
+
+export async function getStaticProps(){
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const data =  await response.json()
+
+  return{
+    props:{
+      posts: data.slice(0,3)
+    }
+  }
+}
