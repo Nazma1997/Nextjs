8b7a5fb3163d36b28b4d4dfa79ce0fd293723c88
Nazma1997 <nazma.ist.707@gmail.com>
2022-10-08 15:18:51 +0600

    added

---
+++ data-fetching/pages/users.js
@@ -0,0 +1,33 @@
+
+
+
+export default function UserList({users}){
+  return(
+    <div>
+      <h1>All User List</h1>
+      {users.map(user => {
+        return(
+          <div key={user.id}>
+            <p>{user.name}</p>
+            <p>User name:{user.username}</p>
+            <p>{user.email}</p>
+
+          </div>
+        )
+      })}
+    </div>
+  )
+};
+
+
+export async function getStaticProps(){
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+  console.log(data)
+
+  return{
+    props:{
+      users:data,
+    },
+  }
+}
